6b23b9d2d4e1bd8a27039380d4b0ba12aa7433f2
sneha <snehamittal88293@gmail.com>
2022-10-23 17:58:48 +0530

    Add url

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const https = require("https");
 const app = express();
 
 app.get("/" , function (req , res){
+
+     const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=734b3d317bd733028f2b25180b0d949d&units=metric"
+     https.get(url , function(response){
+          console.log(response);
+     })
        res.send("Server is up and running");
   });
-
-
 
 app.listen(3000 , function(){
      console.log("Server is running on port 3000.");
