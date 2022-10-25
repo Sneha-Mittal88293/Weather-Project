cd9a2f2c944e467c846b0a6ed9a66ca9d81175a9
sneha <snehamittal88293@gmail.com>
2022-10-25 16:38:59 +0530

    use body=parser to parse post request

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/" , function (req , res){
 
-     const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=734b3d317bd733028f2b25180b0d949d&units=metric"
+     res.sendFile(__dirname + "/index.html");
+
+  });
+
+  app.post("/" , function(req , res){
+       const query = req.body.cityName;
+     const apiKey = "734b3d317bd733028f2b25180b0d949d";
+     const unit = "metric";
+     const url = "https://api.openweathermap.org/data/2.5/weather?q=" +query+"&appid="+apiKey+"&units=" +unit;
      https.get(url , function(response){
           console.log(response.statusCode);
 
@@ -14,18 +25,19 @@ app.get("/" , function (req , res){
                const weatherDescription  = weatherData.weather[0].description;
                const icon = weatherData.weather[0].icon;
                const imageUrl =  "http://openweathermap.org/img/wn/"+icon+"@2x.png";
-               // in that way we send multiple item  
+//                // in that way we send multiple item  
                // res.send("<p>The weather is currently " + weatherDescription+ " </p>\n"+" <h1>The temp of landon is " + temp + " degree celsius</h1>");
                
                // or we can send also that way
                res.write("<p>The weather is currently " + weatherDescription+ " </p>");
-               res.write("<h1>The temp of landon is " + temp + " degree celsius</h1>");
+               res.write("<h1>The temp of " +query+" is"  + temp + " degree celsius</h1>");
                res.write("<img src =" +imageUrl +">");
                res.send();
 
           });
      });
   });
+
 
 app.listen(3000 , function(){
      console.log("Server is running on port 3000.");
